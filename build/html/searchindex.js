41dabfb2ee1b1ca2f29fd7645e9ec352d9a359f9
pjs <fpsping@163.com>
2018-08-18 00:14:23 +0800

    readthedocs

---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["helloworld","index"],envversion:53,filenames:["helloworld.rst","index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to power\u2019s documentation!"],titleterms:{document:1,indic:1,power:1,tabl:1,welcom:1}})
